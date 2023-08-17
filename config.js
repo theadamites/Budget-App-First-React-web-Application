fdd5a8a117d630330dbfb639d93375a066a569d9
theadamites <124354862+theadamites@users.noreply.github.com>
2023-08-16 23:21:36 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwzHoGuQJEav6fehArw0afj8bJZ_0dfgk",
+  authDomain: "easy-budge.firebaseapp.com",
+  databaseURL: "https://easy-budge-default-rtdb.firebaseio.com",
+  projectId: "easy-budge",
+  storageBucket: "easy-budge.appspot.com",
+  messagingSenderId: "345815384557",
+  appId: "1:345815384557:web:3dd790239122401a8d27e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
